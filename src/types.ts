3a51a09f3d5f523f37463bba3c18e54951245bfd
Christophe Eymard <christophe.eymard@sales-way.com>
2018-02-06 17:22:45 +0100

    first import

---
+++ src/types.ts
@@ -0,0 +1,18 @@
+
+export type TopologyTerminal = 'bool' | 'int' | 'number' | 'date'
+
+export type Topology =
+  TopologyTerminal | {[name: string]: Topology}
+
+/**
+ * Records are the objects that flow within our system
+ */
+export interface Record {
+  collection: string
+  topology: Topology
+  fields: any
+}
+
+export interface Event {
+
+}
