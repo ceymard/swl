54bb7fdc3e79f9c04414b2b07da405dd9c5914a1
Christophe Eymard <christophe.eymard@sales-way.com>
2018-02-13 18:09:53 +0100

    ...

---
+++ src/cmdparse.ts
@@ -0,0 +1,21 @@
+
+import * as P from 'parsimmon'
+import {PARSER as OBJ} from './oparse'
+
+export type PipelineContent = string | string[]
+export type Pipeline = PipelineContent[]
+
+const DIVIDER = P.regex(/\s*::\s*/)
+const LBRACKET = P.regex(/\s*\[\s*/)
+const RBRACKET = P.regex(/\s*]\s*/)
+
+const INSTRUCTION = P.regex(/((?!::|[\[\]]).)+/)
+
+const INSTR = P.alt(
+  P.seqMap(LBRACKET, P.lazy(() => PIPE), RBRACKET, (_1, res, _2) => res),
+  INSTRUCTION
+)
+
+const PIPE: P.Parser<Pipeline> = P.sepBy1(INSTR, DIVIDER)
+
+export const PARSER = PIPE
--- src/swl.ts
+++ src/swl.ts
@@ -1,29 +1,33 @@
 #!/usr/bin/env node
-import {PARSER} from './oparse'
+// import {PARSER} from './oparse'
 import {inspect} from 'util'
+import {PARSER} from './cmdparse'
 
-const res = PARSER.parse(`
-  no tls,
-  folder: Amgen/Pouet,
-  user: incoming@salesway.eu,
-  password: 1nc0m1ng_,
-  reg: r'.*[^]+'i,
-  files: [./c, /home/chris, /rtort*],
+// const res = PARSER.parse(`
+//   no tls,
+//   folder: Amgen/Pouet,
+//   user: incoming@salesway.eu,
+//   password: 1nc0m1ng_,
+//   reg: r'.*[^]+'i,
+//   files: [./c, /home/chris, /rtort*],
 
-no zop,
-not dope-there,
-don't discuss,
-ab,
-!acb,
-abc: pouet,
-aaa:  true  ,
-h: [1, 2, 3,  false, {toto: 2}, yes, no ],
-zob: {a: 1, b: -2.54},
-toto: 12,
-d: 2018-01-01 01:01:59
+// no zop,
+// not dope-there,
+// don't discuss,
+// ab,
+// !acb,
+// abc: pouet,
+// aaa:  true  ,
+// h: [1, 2, 3,  false, {toto: 2}, yes, no ],
+// zob: {a: 1, b: -2.54},
+// toto: 12,
+// d: 2018-01-01 01:01:59
 
 
-source1: 'SELECT zobi'
-`)
+// source1: 'SELECT zobi'
+// `)
 
-console.log(inspect(res.status ? res.value.result : 'error', {colors: true}))
+const args = process.argv.slice(2).join(' ')
+console.log(inspect(PARSER.parse(args), {colors: true, depth: null}))
+
+// console.log(inspect(res.status ? res.value.result : 'error', {colors: true}))
