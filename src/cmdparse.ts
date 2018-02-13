2728522ef5c0a08d442467552afca08554d29a3c
Christophe Eymard <christophe.eymard@gmail.com>
2018-02-13 22:03:57 +0100

    ...

--- src/cmdparse.ts
+++ src/cmdparse.ts
@@ -5,11 +5,11 @@ import {PARSER as OBJ} from './oparse'
 export type PipelineContent = string | string[]
 export type Pipeline = PipelineContent[]
 
-const DIVIDER = P.regex(/\s*::\s*/)
+const DIVIDER = P.regex(/\s*\|\s*/)
 const LBRACKET = P.regex(/\s*\[\s*/)
 const RBRACKET = P.regex(/\s*]\s*/)
 
-const INSTRUCTION = P.regex(/((?!::|[\[\]]).)+/)
+const INSTRUCTION = P.regex(/([^\[\]\|]+)/)
 
 const INSTR = P.alt(
   P.seqMap(LBRACKET, P.lazy(() => PIPE), RBRACKET, (_1, res, _2) => res),
--- src/swl.ts
+++ src/swl.ts
@@ -1,33 +1,7 @@
 #!/usr/bin/env node
-// import {PARSER} from './oparse'
+
 import {inspect} from 'util'
 import {PARSER} from './cmdparse'
 
-// const res = PARSER.parse(`
-//   no tls,
-//   folder: Amgen/Pouet,
-//   user: incoming@salesway.eu,
-//   password: 1nc0m1ng_,
-//   reg: r'.*[^]+'i,
-//   files: [./c, /home/chris, /rtort*],
-
-// no zop,
-// not dope-there,
-// don't discuss,
-// ab,
-// !acb,
-// abc: pouet,
-// aaa:  true  ,
-// h: [1, 2, 3,  false, {toto: 2}, yes, no ],
-// zob: {a: 1, b: -2.54},
-// toto: 12,
-// d: 2018-01-01 01:01:59
-
-
-// source1: 'SELECT zobi'
-// `)
-
 const args = process.argv.slice(2).join(' ')
 console.log(inspect(PARSER.parse(args), {colors: true, depth: null}))
-
-// console.log(inspect(res.status ? res.value.result : 'error', {colors: true}))
