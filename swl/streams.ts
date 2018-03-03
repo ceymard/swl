194bd15c0fb98ff0cf131c4ae9d8491251cf1e2f
Christophe Eymard <christophe.eymard@gmail.com>
2018-03-03 14:18:01 +0100

    now using the Lock object

--- swl/streams.ts
+++ swl/streams.ts
@@ -22,36 +22,70 @@ export async function *make_read_creator(uri: string, options: any): Sources {
 }
 
 
+export class Lock<T = void> {
+  private _prom: Promise<T> | null = null
+  private _resolve: null | ((val: T) => void) = null
+  private _reject: null | ((err: any) => void) = null
+
+  get promise(): Promise<T> {
+    if (!this._prom) {
+      this._prom = new Promise<T>((resolve, reject) => {
+        this._resolve = resolve
+        this._reject = reject
+      })
+    }
+    return this._prom
+  }
+
+  private reset() {
+    this._prom = null
+    this._resolve = null
+    this._reject = null
+  }
+
+  resolve(this: Lock<void>): void
+  resolve(value: T): void
+  resolve(value?: T) {
+    if (!this._resolve) {
+      this.promise
+    }
+    const res = this._resolve!
+    this.reset()
+    res(value!)
+  }
+
+  reject(err: any) {
+    if (!this._reject) { this.promise }
+    const rej = this._reject!
+    this.reset()
+    rej(err)
+  }
+}
+
 
 
 export class StreamWrapper<T extends NodeJS.ReadableStream | NodeJS.WritableStream> {
 
-  _error: any = undefined
   _ended: boolean = false
+  ended = new Lock()
+  readable = new Lock()
+  drained = new Lock()
 
-  constructor(public stream: T, public collection?: string) { }
-
-  async resume_once(...events: string[]) {
-    var acc: Function
-    const prom = new Promise((accept) => {
-      acc = accept
-    })
-
-    const em = this.stream
-    function handle() {
-      for (var e2 of events)
-        em.removeListener(e2, handle)
-      acc()
-    }
-
-    for (var e of events) {
-      em.on(e, handle)
-    }
-    em.on('end', () => {
+  constructor(public stream: T, public collection?: string) {
+    stream.on('readable', e => this.readable.resolve())
+    stream.on('end', e => {
       this._ended = true
+      this.ended.resolve()
+      this.readable.resolve()
     })
-
-    return prom
+    stream.on('drain', e => {
+      this.drained.resolve()
+    })
+    stream.on('error', e => {
+      this.ended.reject(e)
+      this.readable.reject(e)
+      this.drained.reject(e)
+    })
   }
 
   /**
@@ -60,16 +94,15 @@ export class StreamWrapper<T extends NodeJS.ReadableStream | NodeJS.WritableStre
   async read<U extends NodeJS.ReadableStream>(this: StreamWrapper<U>) {
     do {
       var res = this.stream.read()
+
       if (res !== null)
         return res
-      if (this._error) {
-        throw this._error
-      }
+
       if (this._ended) {
         // console.log('stream ended')
         return null
       }
-      await this.resume_once('readable', 'end', 'error')
+      await this.readable.promise
     } while (true)
 
   }
@@ -78,7 +111,7 @@ export class StreamWrapper<T extends NodeJS.ReadableStream | NodeJS.WritableStre
    * Write data to the streama
    */
   async write<U extends NodeJS.WritableStream>(this: StreamWrapper<U>, data: any) {
-    await this.resume_once('drain')
+    await this.drained.promise
   }
 
 }
