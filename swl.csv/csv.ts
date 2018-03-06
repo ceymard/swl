797c2f10ab2064f8a3548a69f37165038304ba04
Christophe Eymard <christophe.eymard@sales-way.com>
2018-03-06 11:30:17 +0100

    csv sink is back

--- swl.csv/csv.ts
+++ swl.csv/csv.ts
@@ -1,4 +1,4 @@
-import { URI_WITH_OPTS, make_write_creator, make_read_creator, sources, y, ChunkIterator, Chunk, StreamWrapper} from 'swl'
+import { URI_WITH_OPTS, make_write_creator, make_read_creator, sources, y, ChunkIterator, Chunk, StreamWrapper, sinks} from 'swl'
 
 import * as stringify from 'csv-stringify'
 import * as parse from 'csv-parse'
@@ -34,53 +34,30 @@ sources.add(
     }
 }, '.csv')
 
-/*
-export class CsvOutput extends StreamSink {
 
-  schema = y.object({
+sinks.add(
+  y.object({
     encoding: y.string().default('utf-8'),
     header: y.boolean().default(true),
     delimiter: y.string().default(';')
-  })
+  }),
+  async function csv(opts, rest) {
+    const [uri, options] = URI_WITH_OPTS.tryParse(rest)
 
-  options = this.schema.cast(this.options)
+    return async function *csv(upstream: ChunkIterator): ChunkIterator {
+      var w = await make_write_creator(uri, options)
+      var file: StreamWrapper<NodeJS.WritableStream>
 
-  async codec() {
-    // const opts = this.schema.cast(this.options)
-    return stringify({
-      header: this.options.header,
-      delimiter: this.options.delimiter
-    })
-  }
-
-}
-
-register_sink(async (opts: any, str: string) => {
-  const [uri, options] = URI_WITH_OPTS.tryParse(str)
-  return new CsvOutput(opts, await make_write_creator(uri, options))
-}, 'csv', '.csv')
-
-
-export class CsvSource extends StreamSource {
-
-  schema = y.object({
-    columns: y.boolean().default(true),
-    delimiter: y.string().default(';'),
-    auto_parse: y.boolean().default(true)
-  })
-
-  options = this.schema.cast(this.options)
-
-  async nextSource(source: NodeJS.ReadableStream) {
-    return source.pipe(parse(this.options))
-  }
-
-}
-
-register_source(async (opts: any, rest: string) => {
-  const [uri, options] = URI_WITH_OPTS.tryParse(rest)
-  options.encoding = 'utf-8'
-  // console.log(uri, options)
-  return new CsvSource(opts, await make_read_creator(uri, options || {}))
-}, 'csv', '.csv')
-*/
+      for await (var chk of upstream) {
+        if (chk.type === 'start') {
+          var end = await w(chk.name)
+          var st = stringify(opts)
+          st.pipe(end)
+          file = new StreamWrapper(st)
+        } else if (chk.type === 'data') {
+          await file!.write(chk.payload)
+        } else yield chk
+      }
+    }
+  }, '.csv'
+)
