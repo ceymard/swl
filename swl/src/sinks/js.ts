1411b440bc1cfdca343b58905bc70a869905b7a8
Christophe Eymard <christophe.eymard@sales-way.com>
2018-03-21 18:19:53 +0100

    added jsobj, pending rename

--- swl/src/sinks/js.ts
+++ swl/src/sinks/js.ts
@@ -33,3 +33,36 @@ sinks.add(
     }
   }
 )
+
+
+sinks.add(
+  y.object({}),
+  function jsobj(opts, rest) {
+
+    var fn: Function = eval(`(_, collection, i) => { return {..._, ${rest}} }`)
+
+    return async function* (upstream: ChunkIterator): ChunkIterator {
+      // yield* upstream
+      var collection = ''
+      var i = 0
+      for await (var ch of upstream) {
+        if (ch.type === 'start') {
+          collection = ch.name
+          i = 0
+          yield ch
+        } else if (ch.type === 'data') {
+          i++
+          var res = fn(ch.payload, collection, i)
+          if (res[Symbol.iterator]) {
+            for (var r of res)
+              yield Chunk.data(r)
+          } else if (res[Symbol.asyncIterator]) {
+            for await (var r of res)
+              yield Chunk.data(r)
+          } else yield Chunk.data(res)
+
+        } else yield ch
+      }
+    }
+  }
+)
