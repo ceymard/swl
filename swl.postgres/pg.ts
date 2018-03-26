29aed7b503b676427f47695154f03847fe6719df
Christophe Eymard <christophe.eymard@sales-way.com>
2018-03-26 11:33:51 +0200

    re-added postgres sink

--- swl.postgres/pg.ts
+++ swl.postgres/pg.ts
@@ -1,5 +1,5 @@
 
-import {URI_AND_OBJ, sources, Chunk, ChunkIterator, y} from 'swl'
+import {URI_AND_OBJ, sources, Chunk, ChunkIterator, y, sinks, URI} from 'swl'
 import * as pg from 'pg'
 
 // export type Selector = boolean | string
@@ -50,83 +50,98 @@ sources.add(
 })
 
 
-/*
-export class PostgresSink extends Sink {
+sinks.add(
+  y.object({
+    truncate: y.boolean().default(false).label('Truncate tables before loading'),
+    drop: y.boolean().default(false).label('Drop tables'),
+  }),
+  function postgres(opts, rest) {
+    const uri = URI.tryParse(rest.trim())
+    const mode: 'insert' | 'upsert' | 'update' = 'insert'
 
-  mode: 'insert' | 'upsert' | 'update' = 'insert'
-  db!: S
+    async function* run(db: pg.Client, upstream: ChunkIterator): ChunkIterator {
 
-  constructor(public filename: string, public options: any = {}) {
-    super(options)
-  }
+      var table: string = ''
+      var columns: string[] = []
+      var start = false
+      var text = ''
+      var query_name = ''
 
-  run(options: any, body: string) {
-    this.db.exec(body)
-  }
+      for await (var ev of upstream) {
+        if (ev.type === 'start') {
+          start = true
+          table = ev.name
+        } else if (ev.type === 'data') {
+          var payload = ev.payload
 
-  async *process(): AsyncIterableIterator<PipelineEvent> {
-    var table: string = ''
-    var columns: string[] = []
-    var start = false
-    var stmt: any
+          // Check if we need to create the table
+          if (start) {
+            columns = Object.keys(payload)
+            var types = columns.map(c => typeof payload[c] === 'number' ? 'real'
+            : payload[c] instanceof Buffer ? 'blob'
+            : 'text')
 
-    this.db = new S(this.filename, {})
+            if (opts.drop) {
+              await db.query(`DROP TABLE IF EXISTS "${table}"`)
+            }
 
-    this.db.exec('BEGIN')
+            // Create if not exists ?
+            // Temporary ?
+            await db.query(`
+              CREATE TABLE IF NOT EXISTS "${table}" (
+                ${columns.map((c, i) => `"${c}" ${types[i]}`).join(', ')}
+              )
+            `)
 
-    for await (var ev of this.upstream()) {
-      if (ev.type === 'start') {
-        start = true
-        table = ev.name
-      } else if (ev.type === 'data') {
-        var payload = ev.payload
+            if (mode === 'insert') {
+              query_name = `swl-query-insert-${table}`
+              text = `INSERT INTO "${table}" (${columns.map(c => `"${c}"`).join(', ')})
+              values (${columns.map((c, i) => `$${i + 1}`).join(', ')})`
+              // console.log(sql)
+            }
+            else if (mode === 'upsert') {
+              query_name = `swl-query-insert-${table}`
+              // Should I do some sub-query thing with coalesce ?
+              // I would need some kind of primary key...
+              text = `INSERT OR REPLACE INTO "${table}" (${columns.map(c => `"${c}"`).join(', ')})
+                values (${columns.map((c, i) => `$${i + 1}`).join(', ')})`
+            }
 
-        // Check if we need to create the table
-        if (start) {
-          columns = Object.keys(payload)
-          var types = columns.map(c => typeof payload[c] === 'number' ? 'real'
-          : payload[c] instanceof Buffer ? 'blob'
-          : 'text')
-          // Create if not exists ?
-          // Temporary ?
-          this.db.exec(`
-            CREATE TABLE IF NOT EXISTS "${table}" (
-              ${columns.map((c, i) => `"${c}" ${types[i]}`).join(', ')}
-            )
-          `)
 
-          if (this.mode === 'insert') {
-            const sql = `INSERT INTO "${table}" (${columns.map(c => `"${c}"`).join(', ')})
-            values (${columns.map(c => '?').join(', ')})`
-            // console.log(sql)
-            stmt = this.db.prepare(sql)
+            if (opts.truncate) {
+              await db.query(`DELETE FROM "${table}"`)
+            }
+            start = false
           }
-          else if (this.mode === 'upsert')
-            // Should I do some sub-query thing with coalesce ?
-            // I would need some kind of primary key...
-            stmt = this.db.prepare(`INSERT OR REPLACE INTO "${table}" (${columns.map(c => `"${c}"`).join(', ')})
-              values (${columns.map(c => '?').join(', ')})`)
 
+          await db.query({
+            text,
+            values: columns.map(c => payload[c])
+          })
 
-          if (this.options.truncate) {
-            this.db.exec(`DELETE FROM "${table}"`)
-          }
-          start = false
+        } else if (ev.type === 'exec') {
+          // await (this as any)[ev.method](ev.options, ev.body)
         }
-
-        stmt.run(...columns.map(c => payload[c]))
-      } else if (ev.type === 'exec') {
-        await (this as any)[ev.method](ev.options, ev.body)
       }
+
     }
-    this.db.exec('COMMIT')
+
+    return async function *postgres(upstream: ChunkIterator): ChunkIterator {
+
+      const db = new pg.Client(`postgres://${uri}`)
+      await db.connect()
+
+      try {
+        await db.query('BEGIN')
+        yield* run(db, upstream)
+        await db.query('COMMIT')
+      } catch (e) {
+        await db.query('ROLLBACK')
+        throw e
+      } finally {
+      }
+
+      await db.end()
+    }
   }
-
-}
-
-register_sink(async (opts: any, parse: string) => {
-  const file = URI.tryParse(parse.trim())
-  // console.log(file, sources)
-  return new PostgresSink(file, opts)
-}, 'postgres', 'pg')
-*/
+)
