59801160161c659687fee23b18356b7771476bdd
Christophe Eymard <christophe.eymard@gmail.com>
2019-06-11 17:56:13 +0200

    handle arrays in sqlite

--- swl.sqlite/sqlite.ts
+++ swl.sqlite/sqlite.ts
@@ -243,8 +243,12 @@ const re_date = /^\d{4}-\d{2}-\d{2}(?:T\d{2}:\d{2}(?:\d{2}(?:\.\d{3}Z?)))?$/
 const re_boolean = /^true|false$/i
 
 export function uncoerce(value: any) {
+  if (value && (value[0] === '{' || value[0] === '['))
+    return JSON.parse(value)
+
   if (typeof value === 'string') {
     var trimmed = value.trim().toLowerCase()
+
     if (trimmed.match(re_date)) {
       return new Date(trimmed)
     }
@@ -273,8 +277,8 @@ export function coerce(value: any) {
 
   if (value instanceof Date) {
     return (new Date(value.valueOf() - (value.getTimezoneOffset() * 60000))).toISOString()
-  } if (Array.isArray(value))
-    return value.join(', ')
+  } //if (Array.isArray(value))
+    //return value.join(', ')
 
   return JSON.stringify(value)
 }
